3d32171470b96559db7b8bd06c88fece2066136e
wigalex@live.unc.edu <wigalex@live.unc.edu>
2022-04-07 16:01:07 -0400

    new

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ args["port"]
 const port = args.port || 5000
 
 //const helpMsg = "server.js [options]\n\n--por		Set the port number for the server to listen on. Must be an integer\n           between 1 and 65535.\n\n--debug	If set to true, creates endlpoints /app/log/access/ which returns\n              a JSON access log from the database and /app/error which throws \n              an error with the message \"Error test successful.\" Defaults to \n  false.\n\n--log		If set to false, no log files are written. Defaults to true.\n  Logs are always written to database.\n\n--help	Return this message and exit."
-const helpMsg = (`
+const help = (`
 server.js [options]
 
 --port	Set the port number for the server to listen on. Must be an integer
@@ -30,7 +30,7 @@ server.js [options]
 --help	Return this message and exit.
 `)
 if(args.help || args.h){
-console.log(helpMsg)
+console.log(help)
 process.exit(0)
 }
 //const logging = (req,res,next) => {
